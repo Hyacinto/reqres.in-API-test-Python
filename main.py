74c41d193818b63344850df11eb3c45e1786503b
Hyacinto <jz.juhasz@gmail.com>
2025-05-23 21:32:52 +0200

    Add headers to API requests and update CSV handling in tests, because of the changes of the reqres.api.

--- main.py
+++ main.py
@@ -19,7 +19,12 @@ method_texts = [method.text for method in method_elements]
 path_elements = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//span[@class='opblock-summary-path']")))
 path_texts = [path.text for path in path_elements]
 
+driver.get('https://reqres.in/signup')
+header_value = driver.find_element(By.CSS_SELECTOR, ".code:nth-child(2)").text
+
 driver.quit()
+
+headers = {"x-api-key": header_value}
 
 for i in range(len(path_texts)):
     if "{resource}" in path_texts[i]:
@@ -40,24 +45,29 @@ post_body_noPassword = {
   "password": ""
 }
 
+post_body_noValues = {
+  "email": "",
+  "password": ""
+}
+
 body = None
 
 base_url = "https://reqres.in/api"
 
 with open('data.csv', 'w', newline='', encoding='utf-8') as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(["Method", "Path", "Body", "Result"])
+    writer.writerow(["Method", "Path", "Body", "Header", "Result"])
     for i in range(len(method_texts)):
         if "{id}" in path_texts[i]:
-            writer.writerow([method_texts[i], base_url + path_texts[i].replace("{id}", "1"), body, True])
-            writer.writerow([method_texts[i], base_url + path_texts[i].replace("{id}", "0"), body, False])
-            continue 
+            writer.writerow([method_texts[i], base_url + path_texts[i].replace("{id}", "1"), body, headers, True])
+            writer.writerow([method_texts[i], base_url + path_texts[i].replace("{id}", "0"), body, headers, False])
+            continue
         if method_texts[i] == "POST" and (path_texts[i] == "/login" or path_texts[i] == "/register"):
-            writer.writerow([method_texts[i], base_url + path_texts[i], post_body, True])
-            writer.writerow([method_texts[i], base_url + path_texts[i], post_body_noEmail, False])
-            writer.writerow([method_texts[i], base_url + path_texts[i], post_body_noPassword, False])
+            writer.writerow([method_texts[i], base_url + path_texts[i], post_body, headers, headers, True])
+            writer.writerow([method_texts[i], base_url + path_texts[i], post_body_noEmail, headers, False])
+            writer.writerow([method_texts[i], base_url + path_texts[i], post_body_noPassword, headers, False])
+            writer.writerow([method_texts[i], base_url + path_texts[i], post_body_noValues, headers, False])
         else:
-            writer.writerow([method_texts[i], base_url + path_texts[i], body, True])
+            writer.writerow([method_texts[i], base_url + path_texts[i], body, headers, True])
 
 
-   
--- test_api.py
+++ test_api.py
@@ -12,13 +12,12 @@ def read_csv_data():
                 body = eval(row['Body'])
             else:
                 body = None
-            data.append((row['Method'], row['Path'], body, result))
+            headers = eval(row['Header']) if row['Header'] else {}
+            data.append((row['Method'], row['Path'], body, headers, result))
     return data
 
-@pytest.mark.parametrize("Method,Path,Body,Result", read_csv_data())
-def test_http_requests(Method,Path,Body,Result):
+@pytest.mark.parametrize("Method,Path,Body,Headers,Result", read_csv_data())
+def test_http_requests(Method,Path,Body,Headers,Result):
 
-    response = getattr(requests, Method.lower())(Path, json=Body)
+    response = getattr(requests, Method.lower())(Path, json=Body, headers=Headers)
     assert (response.status_code == 200) == Result or (response.status_code == 204) == Result
-
-
